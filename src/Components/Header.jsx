8372cd482e752a3fa419b6472a591c2f3f131a08
lillyboenig <lilly.boenig@web.de>
2024-10-26 16:10:54 +0300

    Creating the header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = ({ image, title }) => {
+    return (
+        <header className="header">
+            <img src={image} alt="Product Logo" className="header-image" />
+            <h1 className="header-title">{title}</h1>
+        </header>
+    );
+};
+
+export default Header;
