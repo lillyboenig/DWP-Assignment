e24d28b7879ddc2e833eb2683a047211cd51df54
lillyboenig <lilly.boenig@web.de>
2024-10-26 16:15:10 +0300

    Coding the Component-Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+import Header from './Components/Header';
+import ProductForm from './Components/ProductForm';
+import OrderInfo from './Components/OrderInfo';
+import productImage from './images/product-image.jpeg';
+
 import './App.css'
 
 function App() {
@@ -9,6 +14,41 @@ function App() {
       
     </>
   )
+}
+
+function ComponentExercise() {
+  const products = [
+      { name: "Spooky Mushroom", price: 10.0 },
+      { name: "Fancy Mushroom", price: 20.0 },
+      { name: "Deluxe Mushroom", price: 30.0 },
+  ];
+
+  const [selectedProductIndex, setSelectedProductIndex] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const handleProductChange = (index) => {
+      setSelectedProductIndex(index);
+  };
+
+  const handleQuantityChange = (newQuantity) => {
+      setQuantity(newQuantity);
+  };
+
+  return (
+      <div>
+          <Header image={productImage} title="Mushroom Store" />
+          <ProductForm
+              products={products}
+              onProductChange={handleProductChange}
+              onQuantityChange={handleQuantityChange}
+          />
+          <OrderInfo
+              productName={products[selectedProductIndex].name}
+              price={products[selectedProductIndex].price}
+              quantity={quantity}
+          />
+      </div>
+  );
 }
 
 export default App
